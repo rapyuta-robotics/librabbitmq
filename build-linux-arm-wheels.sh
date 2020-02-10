878e1b70a49c31657b8bf1fba50c8bd261b74694
Shivam Mamgain <shivamMg@users.noreply.github.com>
2020-02-10 21:07:05 +0530

    Add wheel for arm7l

--- build-linux-arm-wheels.sh
+++ build-linux-arm-wheels.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# works on ubuntu
+# tested on ubuntu 16.04 (arm7l and aarch64)
 # must be executed as ./build-linux-arm-wheels.sh to capture CWD
 set -e -x
 
@@ -15,5 +15,6 @@ cd /tmp
 virtualenv env
 . env/bin/activate
 pip wheel ${CWD} -w ${CWD}/wheelhouse/
-pip install ${CWD}/wheelhouse/librabbitmq-2.0.0-cp27-cp27mu-manylinux2014_armv7l.whl
+ARCH=$(python -c 'import platform; print(platform.machine())')
+pip install ${CWD}/wheelhouse/librabbitmq-2.0.0-cp27-cp27mu-linux_${ARCH}.whl
 python -c "import librabbitmq"
